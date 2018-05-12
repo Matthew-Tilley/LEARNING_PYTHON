0c3564065428e87e135a98e5e4f1a3bb61daee23
Matt Tilley <mtilley255@gmail.com>
2018-05-12 11:28:52 -0500

    02.04 Created test code per example / executed.

--- Ex_Files_Learning_Python/Exercise Files/Ch2/conditionals_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch2/conditionals_start.py
@@ -5,7 +5,14 @@
 def main():
   x, y = 10, 100
   
-  # conditional flow uses if, elif, else  
+  # conditional flow uses if, elif, else
+  
+  if (x < y):
+    st = "X is less than Y"
+  print(st)  
+
+      
+
 
   # conditional statements let you use "a if C else b"
   
