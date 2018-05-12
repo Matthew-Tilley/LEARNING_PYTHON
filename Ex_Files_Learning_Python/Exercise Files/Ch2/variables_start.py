3a0faeb279bd890620cdd2e0530a4567ed88d62a
Matt Tilley <mtilley255@gmail.com>
2018-05-12 10:11:35 -0500

    02.02 Placed numerals in the str() function.

--- Ex_Files_Learning_Python/Exercise Files/Ch2/variables_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch2/variables_start.py
@@ -11,7 +11,7 @@ f="abc"
 print(f)
 
 # # ERROR: variables of different types cannot be combined
-print("This is a string" + 123)
+print("This is a string" + str(123) )
 
 # Global vs. local variables in functions
 
